d9ed447487f4a61b23fc01a413d232f33afe6c4f
nabeel0209 <muhammadnabeel0209@gmail.com>
2026-04-22 00:03:52 +0500

    Added Breeze Authentication System

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,26 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\studentController;
-
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/students', [studentController::class, 'getStudents']);
-Route::post('/add-students', [studentController::class, 'addStudents']);
-Route::get('/students/edit/{id}', [studentController::class, 'edit']);
-Route::post('/students/update/{id}', [studentController::class, 'updateStudents']);
-Route::get('/students/delete/{id}', [studentController::class, 'deleteStudents']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/students', [studentController::class, 'getStudents']);
+    Route::post('/add-students', [studentController::class, 'addStudents']);
+    Route::get('/students/edit/{id}', [studentController::class, 'edit']);
+    Route::post('/students/update/{id}', [studentController::class, 'updateStudents']);
+    Route::get('/students/delete/{id}', [studentController::class, 'deleteStudents']);
+});
+
+require __DIR__ . '/auth.php';
